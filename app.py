9d1d1bc473021d65092e6ce0d108d2f64a21717e
davibcavalcante <davib506@gmail.com>
2024-11-02 12:48:37 -0300

    Testando novas importações

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
-from .app.config import Config
+from app.config import Config
 from app.routes import routes_blueprint
 from flask_cors import CORS
 
@@ -8,7 +8,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     CORS(app)
-    jwt = JWTManager(app)
 
     app.register_blueprint(routes_blueprint)
     
