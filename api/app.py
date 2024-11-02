50a69a77d8fc8b04cd4c4be5d4a0e82b4c3885af
davibcavalcante <davib506@gmail.com>
2024-11-02 14:47:19 -0300

    Teste

--- api/app.py
+++ api/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-from .config import Config
-from .routes import routes_blueprint
+# from .config import Config
+# from .routes import routes_blueprint
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    # app.config.from_object(Config)
     CORS(app)
 
-    app.register_blueprint(routes_blueprint)
+    # app.register_blueprint(routes_blueprint)
     
     @app.route('/')
     def home():
