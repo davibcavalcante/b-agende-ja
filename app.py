27bdf79d3eb8a8640f3762fc3064a3a2b83de223
davibcavalcante <davib506@gmail.com>
2024-11-02 13:09:38 -0300

    Corrigindo importação de módulos e arquivos

--- app.py
+++ app.py
@@ -1,23 +1,6 @@
-from flask import Flask, jsonify
-from flask_jwt_extended import JWTManager
-from app.config import Config
-from app.routes import routes_blueprint
-from flask_cors import CORS
+from app import create_app
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    CORS(app)
-
-    app.register_blueprint(routes_blueprint)
-    
-    @app.route('/')
-    def home():
-        return jsonify(message="Servidor rodando!")
-
-    return app
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
-
     app.run()
--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
-from flask import Flask
-from config import Config
-from routes import routes_blueprint
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+from .config import Config
+from .routes import routes_blueprint
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    CORS(app)
+
     app.register_blueprint(routes_blueprint)
+    
+    @app.route('/')
+    def home():
+        return jsonify(message="Servidor rodando!")
+
     return app
